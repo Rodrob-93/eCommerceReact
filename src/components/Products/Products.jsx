ff3f3a628864c85d590a74cf3fe3850c302bd91c
Rodrob-93 <robinrodrigues6499@gmail.com>
2021-04-21 21:54:29 +0530

    Added a lot of components

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,30 +1,27 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
 
 import Product from './Product/Product';
 import useStyles from './styles';
 
-const products = [
-    { id: 1, name: 'Shoes', description: 'Running shoes.', price: '$5', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZzeRACAIE9PEzFodedXBsvlA1w5FdWr1UZA&usqp=CAU'},
-    { id: 2, name: 'Macbook', description: 'Apple macbook.', price: '$10', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwreJPA9T_u9v4DeGKKu9s_I1hvyfaM35JLapuLyb4GnnoDwbwYi0NgW926BzzO6aWfovzXO4&usqp=CAc'},
-];
+const Products = ({ products, onAddToCart }) => {
+  const classes = useStyles();
 
-const Products = () => {
-    const classes = useStyles();
+  if (!products.length) return <p>Loading...</p>;
 
-    return(
-        <main className={classes.content}>
-            <div className={classes.toolbar} />
-            <Grid container justify="center" spacing={4}>
-                {products.map((product) => (
-                    <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-                        <Product product={product} />
-                    </Grid>    
-                ))}
-            </Grid>
-        </main>
-    )
-    
-}
- 
+  return (
+    <main className={classes.content}>
+      <div className={classes.toolbar} />
+      <Grid container justify="center" spacing={4}>
+        {products.map((product) => (
+          <Grid key={product.id} item xs={12} sm={6} md={4} lg={3}>
+            <Product product={product} onAddToCart={onAddToCart} />
+          </Grid>
+        ))}
+      </Grid>
+    </main>
+  );
+};
+
 export default Products;
+
